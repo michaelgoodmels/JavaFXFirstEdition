add6547aff24f1d5433700ff94451bf8be887210
michaelgoodmels <git@good.li>
2024-10-17 13:31:32 +0200

    update label with text

--- JavaFXFirstEdition/src/main/java/com/example/javafxfirstedition/JavaFXFirstEditionController.java
+++ JavaFXFirstEdition/src/main/java/com/example/javafxfirstedition/JavaFXFirstEditionController.java
@@ -8,18 +8,23 @@ public class JavaFXFirstEditionController {
     private Label SecretText01;
 
     @FXML
+    private Label SecretText02;  // Neues Label hinzugefügt
+
+    @FXML
     protected void onSecret01() {
         SecretText01.setText("I'm a Sworbler");
+        SecretText02.setText("dörfen alli wüsse");  // Setze Text im zweiten Label
     }
 
     @FXML
     protected void onSecret02() {
         SecretText01.setText("I'm a Putin understander");
+        SecretText02.setText("aber pssst, niemertem verzelle");  // Setze Text im zweiten Label
     }
 
     @FXML
     protected void onSecret03() {
         SecretText01.setText("I'm not vaccinated");
+        SecretText02.setText("aber pssst, niemertem verzelle");  // Setze Text im zweiten Label
     }
 }
-
